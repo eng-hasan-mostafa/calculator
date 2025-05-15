4e4bc9bbdf21a1caa4b3dc2dd0e3fd6e53deecdb
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-05-15 18:58:32 +0300

    Create the calculator operation logic
    A calculator operation consists of two numbers and an operator.
    Create a new function called `operate` that takes an operator and
    two numbers as parameters. This function should then call one of
    the basic mathematical operation functions to perform the calculation
    using the two numbers.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return +a / +b;
 }
+
+function operate(firstOperand, operator, secondOperand) {
+    let result = null;
+    switch(operator) {
+        case '+':
+            result = add(firstOperand, secondOperand);
+            break;
+        case '-':
+            result = subtract(firstOperand, secondOperand);
+            break;
+        case '*':
+            result = multiply(firstOperand, secondOperand);
+            break;
+        case '/':
+             result = divide(firstOperand,secondOperand);
+             break;
+    }
+    return result;
+}
