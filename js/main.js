483e542fe78f235b90daf5f23061f9509c2a37ab
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-05-16 19:06:25 +0300

    Add functionality to digit buttons
    Create the functions that populate the display when you click
    the digit buttons.
    Create three variables, one for each part of the operation.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if(b == 0) return '';
     return +a / +b;
 }
 
@@ -33,3 +34,33 @@ function operate(firstOperand, operator, secondOperand) {
     }
     return result;
 }
+
+let firstOPerand = '';
+let secondOPerand = '';
+let operator = '';
+let isFirstOperandTurn = true;
+
+function updateDisplayScreenContent(content) {
+    let displayScreen = document.querySelector('.display-screen');
+    displayScreen.textContent = content;
+}
+
+let numberButtons = document.querySelectorAll('.number-btn');
+numberButtons.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        let clickedDigit = e.target.value;
+        let number = (isFirstOperandTurn? firstOPerand: secondOPerand);
+
+        if(clickedDigit !== '.') {
+            number += clickedDigit;
+        }else if(firstOPerand) {
+            number += clickedDigit;
+        }
+        updateDisplayScreenContent(number);
+        (isFirstOperandTurn?
+         firstOPerand = number:
+         secondOPerand = number
+        );
+    });
+});
+
